d5f2c1b48d0b1f20dbda1a6ae8851ccc85083fed
marely <marely.carcamo@gmail.com>
2024-02-17 21:19:07 -0300

    version 1.0

---
+++ js/galeria-pokemon.js
@@ -0,0 +1,35 @@
+async function capturaDatos(i) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + i;
+    let respuesta = await fetch(url);
+    let datosPokemon = await respuesta.json();
+    let urlPokemon = datosPokemon.sprites.other.dream_world.front_default; //url imagen pokemon
+    let numeroPokemon = `#${datosPokemon.id}`;  // número de pokemon
+    let nombrePokemon = `${datosPokemon.name}`;  // nombre de pokemon
+
+    // Crear la tarjeta del Pokémon
+    let tarjeta = `
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2  mb-4">
+            <div class="card shadow h-100 d-flex flex-column mx-1">
+                <div class="d-flex align-items-end justify-content-center" style="height:220px; overflow:hidden;">
+                <img src="${urlPokemon}" class="card-img-top img-fluid px-3"  style="width:auto; height:80%; " alt="${nombrePokemon}">
+                </div> 
+                <div class="card-body">
+                    <h5 class="card-title text-center">${nombrePokemon}</h5>
+                    <p class="card-text text-center">${numeroPokemon}</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+
+    
+    // Agregar la tarjeta a la galería
+    $("#galeria").append(tarjeta);
+}
+
+$(document).ready(function(){
+    // Obtener los datos de los primeros 150 Pokémon
+    for (let i = 1; i <= 150; i++) {
+        capturaDatos(i);
+    }
+});
